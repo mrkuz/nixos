e93f57c1d2f87b6268a9f594c306d95741638974
mrkuz <markus@bitsandbobs.net>
2020-08-04 19:23:35 +0200

    Add configuration file

---
+++ configuration.nix
@@ -0,0 +1,259 @@
+{ config, lib, pkgs, ... }:
+
+let
+  stable = import <nixos-stable> {};
+  home-manager = import <home-manager> {};
+
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+
+  nixos-packages = pkgs.writeShellScriptBin "nixos-packages" ''
+    nix-instantiate --strict --json --eval -E 'builtins.map (p: p.name) (import <nixpkgs/nixos> {}).config.environment.systemPackages' | nix run nixpkgs.jq -c jq -r '.[]' | sort -u
+  '';
+
+  command-not-found = pkgs.writeShellScriptBin "command-not-found" ''
+    echo "$1: command not found"
+  '';
+
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Boot loader
+  boot.loader = {
+    efi.canTouchEfiVariables = false;
+    grub.device = "nodev";
+    generationsDir.enable = true;
+    timeout = 5;
+  };
+
+  # Networking
+  networking = {
+    hostName = "nixos";
+    interfaces.wlp2s0.useDHCP = true;
+  };
+
+  # Localization
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Vienna";
+
+  # SMART
+  services.smartd.enable = false;
+
+  # Filesystem
+  swapDevices = [ { device = "/dev/vg00/swap"; } ];
+  fileSystems."/data/user" = {
+    device = "/dev/vg00/user";
+    fsType = "ext4";
+  };
+
+  # Graphics
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "nvidia" ];
+  };
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.nvidia.nvidiaPersistenced = true;
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
+  # Sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Printing
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.hplipWithPlugin ];
+  };
+
+  # Scanning
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.hplipWithPlugin ];
+  };
+
+  # Miscellanous hardware
+  hardware.steam-hardware.enable = true;
+
+  # Security
+  # security.pam.enableEcryptfs = true;
+
+  # Virtualization
+  virtualisation = {
+    # anbox.enable = true;
+    docker.enable = true;
+    # docker.enableNvidia = true;
+    virtualbox.host.enable = true;
+    virtualbox.host.enableExtensionPack = true;
+    virtualbox.host.headless = true;
+  };
+
+  # Gnome
+  services.xserver = {
+    desktopManager.gnome3.enable = true;
+    displayManager.gdm.enable = true;
+  };
+  services.gnome3 = {
+    core-utilities.enable = false;
+    chrome-gnome-shell.enable = true;
+    gnome-keyring.enable = true;
+  };
+  security.pam.services.gdm.enableGnomeKeyring = true;
+  programs = {
+    evince.enable = true;
+    file-roller.enable = true;
+    gnome-terminal.enable = true;
+    seahorse.enable = true;
+  };
+
+  # Flatpak
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
+
+  # fish
+  programs.fish.enable = true;
+
+  # Android
+  programs.adb.enable = true;
+  services.udev.packages = [
+    pkgs.android-udev-rules
+  ];
+
+  # Packages
+  # nix.nixPath = [ "nixpkgs=/nix/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" ];
+  nixpkgs.config.allowUnfree = true;
+  programs.chromium.enable = true;
+  programs.command-not-found.enable = false;
+  environment.systemPackages = with pkgs; [
+    # Tools
+    dos2unix
+    git
+    htop
+    mkpasswd
+    nmap
+    p7zip
+    python38Packages.ansible
+    python38Packages.ansible-lint
+    python38Packages.wakeonlan
+    pwgen
+    rclone
+    ripgrep
+    rsync
+    tmux
+    unrar
+    unzip
+    vim
+    # Gnome core utilities
+    baobab
+    gnome3.eog
+    gnome3.gedit
+    gnome3.gnome-calculator
+    gnome3.gnome-calendar
+    gnome3.gnome-characters
+    gnome3.gnome-clocks
+    gnome3.gnome-font-viewer
+    gnome3.gnome-screenshot
+    gnome3.gnome-software
+    gnome3.gnome-system-monitor
+    gnome3.gnome-weather
+    gnome3.nautilus
+    simple-scan
+    # Gnome utilities
+    gnome3.gnome-tweaks
+    gnome3.libgnome-keyring
+    # Gnome extensions
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
+    # Default applications
+    google-chrome
+    thunderbird
+    # Applications
+    gimp
+    gparted
+    remmina
+    vlc
+    # Ubuntu look & feel
+    yaru-theme
+    # Docker
+    docker-compose
+    fuse-overlayfs
+    # Virtualizaton
+    vagrant
+    # X utilities
+    glxinfo
+    vulkan-tools
+    weston
+    xclip
+    xwayland
+    # Base packages
+    bridge-utils
+    cifs-utils
+    dnsmasq
+    ecryptfs
+    file
+    lm_sensors
+    openvpn
+    pciutils
+    psmisc
+    python38Packages.cgroup-utils
+    python38Packages.pip
+    smartmontools
+    tuptime
+    update-systemd-resolved
+    # Build essentials
+    cmake
+    gcc
+    gnumake
+    # NixOS
+    home-manager.home-manager
+    # Scripts
+    command-not-found
+    nixos-packages
+    nvidia-offload
+  ];
+
+  # Fonts
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+    noto-fonts
+    source-code-pro
+    ubuntu_font_family
+  ];
+
+  # Users
+  users = {
+    mutableUsers = false;
+    groups.markus.gid = 1000;
+    users = {
+      markus = {
+        uid = 1000;
+        description = "Markus";
+        isNormalUser = true;
+	group = "markus";
+        extraGroups = [ "wheel" "docker" "adbusers" "lp" "scanner" "vboxusers" ];
+        hashedPassword = "$6$mFkTicworPz1frRb$L4ZpTQKCtWdsmndgunILqG9u6cy2qaF9PmxP3DGulpxnLbpDZpm98s/sxLHLDoDbogP8NwRdJM/IMdBckug0N/";
+        shell = pkgs.fish;
+      };
+
+      root = {
+        packages = [ pkgs.emacs ];
+        hashedPassword = "*";
+      };
+    };
+  };
+
+  system.stateVersion = "20.03";
+}
