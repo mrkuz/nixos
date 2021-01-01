4dcd16498a2341181ec5c3589e8e262e19ea0cc7
mrkuz <markus@bitsandbobs.net>
2021-01-01 18:24:38 +0100

    Remove repos

--- repos/dotfiles/.config/fish/functions/note.fish
+++
@@ -1,7 +0,0 @@
-function note
-  if test (count $argv) -eq 0
-    echo "Usage: note TITLE"
-  else
-    emacsclient -n -c -F '((name . "org-protocol-capture"))' "org-protocol://capture://N/$argv"
-  end
-end
--- repos/dotfiles/.config/fish/functions/todo.fish
+++
@@ -1,7 +0,0 @@
-function todo
-  if test (count $argv) -eq 0
-    echo "Usage: todo TITLE"
-  else
-    emacsclient -n -c -F '((name . "org-protocol-capture"))' "org-protocol://capture://b/$argv"
-  end
-end
