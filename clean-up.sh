6350158b34d3b98beb314d6e7cbad70020486330
mrkuz <markus@bitsandbobs.net>
2021-01-14 18:21:34 +0100

    Update clean-up.sh

--- clean-up.sh
+++ clean-up.sh
@@ -6,7 +6,9 @@ KEEP_GENERATIONS="2"
 
 nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/$USER/home-manager
 nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/$USER/channels
+nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/$USER/profile
 nix-env --delete-generations +"$KEEP_GENERATIONS" --profile $HOME/.nix-profile
+sudo nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/root/channels
 sudo nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/system
 
 sudo nix-collect-garbage
