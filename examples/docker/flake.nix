b0ab9ea0ec814ef5d7e64f5fc08a91465b1b2e03
mrkuz <markus@bitsandbobs.net>
2021-05-24 11:27:18 +0200

    Add example nix builds

---
+++ examples/docker/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "path:/nix/channels/nixos";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+      defaultPackage."${system}" = pkgs.dockerTools.buildImage {
+        name = "hello-docker";
+        tag = "latest";
+        contents = with pkgs; [ bash coreutils findutils which ];
+        config = {
+          Cmd = [ "${pkgs.bash}/bin/bash" ];
+        };
+      };
+    };
+}
