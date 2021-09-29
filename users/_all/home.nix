a3aa0a6f610b66374816cba0e6dec8f0a46e2433
mrkuz <markus@bitsandbobs.net>
2021-09-29 08:56:12 +0200

    Link .nix-defexpr/nixos to /nix/channels/nixos

--- users/_all/home.nix
+++ users/_all/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [
@@ -37,4 +37,14 @@
       ];
     };
   };
+
+  home.activation.channels = lib.hm.dag.entryAfter [ "writeBoundary" ]
+    ''
+    [ -e $HOME/.nix-defexpr ] || mkdir $HOME/.nix-defexpr
+    rm -f $HOME/.nix-defexpr/channels
+    touch $HOME/.nix-defexpr/channels
+    rm -f $HOME/.nix-defexpr/channels_root
+    touch $HOME/.nix-defexpr/channels_root
+    [ -e $HOME/.nix-defexpr/nixos ] || ln -svf /nix/channels/nixos $HOME/.nix-defexpr/nixos
+    '';
 }
