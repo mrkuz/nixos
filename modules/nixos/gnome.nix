9ce9f13e9825f2769b2db985cbaab43450ab830a
mrkuz <markus@bitsandbobs.net>
2021-10-20 22:17:36 +0200

    Disable pop-shell extension

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -60,7 +60,7 @@ in {
       (callPackage ../../pkgs/desktops/gnome/extensions/dynamic-panel-transparency {})
       (callPackage ../../pkgs/desktops/gnome/extensions/instant-workspace-switcher {})
       (callPackage ../../pkgs/desktops/gnome/extensions/just-perfection {})
-      (callPackage ../../pkgs/desktops/gnome/extensions/pop-shell {})
+      # (callPackage ../../pkgs/desktops/gnome/extensions/pop-shell {})
       (callPackage ../../pkgs/desktops/gnome/extensions/switcher {})
       (callPackage ../../pkgs/desktops/gnome/extensions/workspaces-bar {})
       # Ubuntu look & feel
