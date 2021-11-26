4f911b11c22936f77cae5807a4ede696bd38c7fd
mrkuz <markus@bitsandbobs.net>
2021-11-26 11:07:13 +0100

    Update

--- modules/home-manager/java-packages.nix
+++ modules/home-manager/java-packages.nix
@@ -14,7 +14,7 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       eclipse-mat
-      ((pkgs.gradleGen.override { java = jdk; }).gradle_latest)
+      gradle
       jdk
       maven
       visualvm
