5b8613d2d2e27c582f4bfee077950be34682386b
mrkuz <markus@bitsandbobs.net>
2021-12-15 17:05:54 +0100

    Show new options and versions after rebuild (2)

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+set -e
+
+function clean() {
+  rm ./result
+  rm tmp/packages.*
+}
+
+trap clean EXIT
+
 if [[ $# -ne 1 ]]; then
   echo "Usage: $0 HOST"
   exit 1;
@@ -10,15 +20,14 @@ fi
 sudo nixos-rebuild -vv --keep-going -j 2 --flake ".#$1" build
 
 OPTIONS="etc/nixos/options.json"
-PACKAGES="etc/nixos/system-packages"
-
 if [[ -e "/$OPTIONS" ]]; then
   diff -u --color=always <(jq keys "/$OPTIONS") <(jq keys "result/$OPTIONS") | less
 fi
 
 if [[ -e "/$PACKAGES" ]]; then
-  diff -u --color=always "/$PACKAGES" "result/$PACKAGES" | less
+  nix-store --query --requisites /run/current-system/ | cut -d- -f2- | sort | uniq > tmp/packages.old
+  nix-store --query --requisites result/ | cut -d- -f2- | sort | uniq > tmp/packages.new
+  diff -u --color=always tmp/packages.{old,new} | less
 fi
 
-rm ./result
 sudo nixos-rebuild -j 2 --flake ".#$1" switch
