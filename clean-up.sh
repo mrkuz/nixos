a3f912e2642f4aefb1ca3e2b8d67bb069aebf176
mrkuz <markus@bitsandbobs.net>
2022-02-21 20:34:48 +0100

    Handle all users in clean-up.sh

--- clean-up.sh
+++ clean-up.sh
@@ -4,11 +4,23 @@ set -e
 
 KEEP_GENERATIONS="1"
 
-nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/$USER/home-manager
-nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/$USER/channels
-nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/$USER/profile
-nix-env --delete-generations +"$KEEP_GENERATIONS" --profile $HOME/.nix-profile
-sudo env "PATH=$PATH" nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/per-user/root/channels
-sudo env "PATH=$PATH" nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/system
+function clean() {
+    profile="$1"
+    if [[ -d "$profile" ]]; then
+        echo ">>> $profile"
+        sudo env "PATH=$PATH" nix-env --delete-generations +"$KEEP_GENERATIONS" --profile $profile
+    fi
+}
+
+for path in /nix/var/nix/profiles/per-user/*; do
+    user=${path##*/}
+    clean /nix/var/nix/profiles/per-user/$user/home-manager
+    clean /nix/var/nix/profiles/per-user/$user/channels
+    clean /nix/var/nix/profiles/per-user/$user/profile
+    clean /home/$user/.nix-profile
+done
+
+clean /root/.nix-profile
+clean /nix/var/nix/profiles/system
 
 sudo env "PATH=$PATH" nix-collect-garbage
