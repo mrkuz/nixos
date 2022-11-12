611aa22f4e7f038a879f1295c13a08608a481a41
mrkuz <markus@bitsandbobs.net>
2022-11-12 18:53:33 +0100

    Remove vscode-extensions packages

--- add-vscode-extension.sh
+++ add-vscode-extension.sh
@@ -9,16 +9,3 @@ name=${1##*.}
 version=${2:-latest}
 
 niv add "vscode:$name" -a name=$name -a publisher=$publisher -v $version -t "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/<publisher>/vsextensions/<name>/<version>/vspackage"
-
-mkdir -p ./pkgs/misc/vscode-extensions/$name/
-cat > ./pkgs/misc/vscode-extensions/$name/default.nix <<EOF
-{ pkgs, ...}:
-
-let
-  sources = import ../../../../nix/sources.nix;
-in pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-  mktplcRef = {
-    inherit (sources."vscode:$name") name publisher version sha256;
-  };
-}
-EOF
