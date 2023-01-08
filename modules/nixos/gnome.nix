97958e1f938f93ec2a63c57be6f2981f8285d6bd
mrkuz <markus@bitsandbobs.net>
2023-01-08 13:04:48 +0100

    Re-add quick settings tweaks extension
    This reverts commit 62aaac25f00ef06780b4fdaf81c51427a48a52ad.

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -78,6 +78,7 @@ in
       (callPackage ../../pkgs/desktops/gnome/extensions/dynamic-panel-transparency { })
       (callPackage ../../pkgs/desktops/gnome/extensions/instant-workspace-switcher { })
       (callPackage ../../pkgs/desktops/gnome/extensions/just-perfection { })
+      (callPackage ../../pkgs/desktops/gnome/extensions/quick-settings-tweaks { })
       (callPackage ../../pkgs/desktops/gnome/extensions/workspaces-bar { })
       # Ubuntu look & feel
       yaru-theme
