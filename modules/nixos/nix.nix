22bc0632c0ba10b3a0bc50f657ceb541e12dcc72
mrkuz <markus@bitsandbobs.net>
2023-04-23 15:07:30 +0200

    Introduce minimal option to nix module

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -10,35 +10,51 @@ in
       default = false;
       type = types.bool;
     };
+    minimal = mkOption {
+      default = false;
+      type = types.bool;
+    };
   };
 
-  config = mkIf cfg.enable {
-    nix = {
-      package = pkgs.nix;
-      extraOptions = ''
-        experimental-features = nix-command flakes
-        narinfo-cache-positive-ttl = 86400
-        keep-outputs = true
-        keep-derivations = true
-        auto-optimise-store = true
-      '';
+  config = mkIf cfg.enable (mkMerge [
+    {
+      nix = {
+        package = pkgs.nix;
+        extraOptions = ''
+          experimental-features = nix-command flakes
+          narinfo-cache-positive-ttl = 86400
+          keep-outputs = true
+          keep-derivations = true
+          auto-optimise-store = true
+        '';
 
-      settings = {
-        sandbox = true;
-        substituters = [
-          # "https://cache.nixos.org/"
-          "https://nix-community.cachix.org"
-          "https://nixpkgs-wayland.cachix.org"
-        ];
-        trusted-public-keys = [
-          # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-        ];
+        settings = {
+          sandbox = true;
+          substituters = [
+            # "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+            "https://nixpkgs-wayland.cachix.org"
+          ];
+          trusted-public-keys = [
+            # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+          ];
+        };
       };
 
+      systemd.services.nix-daemon.enable = true;
+      systemd.sockets.nix-daemon.enable = true;
+
+      system.name = systemName;
+      system.stateVersion = vars.stateVersion;
+      system.configurationRevision = self.rev or "dirty";
+
+    }
+    (mkIf (!cfg.minimal) {
       # Use local nixpkgs
-      registry.nixpkgs = {
+
+      nix.registry.nixpkgs = {
         from = {
           id = "nixpkgs";
           type = "indirect";
@@ -48,74 +64,67 @@ in
           type = "path";
         };
       };
-    };
 
-    systemd.services.nix-daemon.enable = true;
-    systemd.sockets.nix-daemon.enable = true;
+      systemd.tmpfiles.rules = [
+        "d   /root/.nix-defexpr        0755  root  root  -  -"
+        "L+  /root/.nix-defexpr/nixos     -     -        -  -  ${nixpkgs}"
+        "L+  /root/.nix-defexpr/nixpkgs   -     -        -  -  ${nixpkgs}"
+      ];
 
-    systemd.tmpfiles.rules = [
-      "d   /root/.nix-defexpr        0755  root  root  -  -"
-      "L+  /root/.nix-defexpr/nixos     -     -        -  -  ${nixpkgs}"
-      "L+  /root/.nix-defexpr/nixpkgs   -     -        -  -  ${nixpkgs}"
-    ];
+      # Provide compatibility layer for non-flake utils
+      environment.etc."nixos/compat/default.nix".text = ''
+        { ... }:
 
-    # Provide compatibility layer for non-flake utils
-    environment.etc."nixos/compat/default.nix".text = ''
-      { ... }:
+        let
+          nixpkgs = import ${nixpkgs} {};
+        in
+        nixpkgs
+      '';
 
-      let
-        nixpkgs = import ${nixpkgs} {};
-      in
-      nixpkgs
-    '';
+      environment.etc."nixos/compat/nixos/default.nix".text = ''
+        { ... }:
 
-    environment.etc."nixos/compat/nixos/default.nix".text = ''
-      { ... }:
+        let
+          current = import ${self};
+        in
+        current.nixosConfigurations."${systemName}"
+      '';
 
-      let
-        current = import ${self};
-      in
-      current.nixosConfigurations."${systemName}"
-    '';
-
-    environment.etc."nixos/configuration.nix".text = ''
-      nix = {
-        nixPath = [ "nixpkgs=${nixpkgs}" ];
-      };
-
-      system.name = "${systemName}";
-      system.stateVersion = "${vars.stateVersion}";
-    '';
-
-    environment.etc."nixos/current".source = self;
-    environment.etc."nixos/nixpkgs".source = nixpkgs;
-    environment.etc."nixos/options.json".source =
-      if config.documentation.nixos.enable
-      then "${config.system.build.manual.optionsJSON}/share/doc/nixos/options.json"
-      else
-        pkgs.writeTextFile {
-          name = "options.json";
-          text = "{}";
+      environment.etc."nixos/configuration.nix".text = ''
+        nix = {
+          nixPath = [ "nixpkgs=${nixpkgs}" ];
         };
 
-    environment.etc."nixos/system-packages".text =
-      let
-        packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-        sorted = builtins.sort (a: b: lib.toLower a < lib.toLower b) (lib.unique packages);
-        formatted = builtins.concatStringsSep "\n" sorted;
-      in
-      formatted;
+        system.name = "${systemName}";
+        system.stateVersion = "${vars.stateVersion}";
+      '';
 
-    environment.extraInit = ''
-      export NIX_PATH="nixpkgs=${nixpkgs}"
-    '';
+      environment.etc."nixos/current".source = self;
+      environment.etc."nixos/nixpkgs".source = nixpkgs;
+      environment.etc."nixos/options.json".source =
+        if config.documentation.nixos.enable
+        then "${config.system.build.manual.optionsJSON}/share/doc/nixos/options.json"
+        else
+          pkgs.writeTextFile {
+            name = "options.json";
+            text = "{}";
+          };
 
-    environment.systemPackages = with pkgs; [
-      nixos-option
-    ];
+      environment.etc."nixos/system-packages".text =
+        let
+          packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+          sorted = builtins.sort (a: b: lib.toLower a < lib.toLower b) (lib.unique packages);
+          formatted = builtins.concatStringsSep "\n" sorted;
+        in
+        formatted;
 
-    system.name = systemName;
-    system.stateVersion = vars.stateVersion;
-    system.configurationRevision = self.rev or "dirty";
-  };
+      environment.extraInit = ''
+        export NIX_PATH="nixpkgs=${nixpkgs}"
+      '';
+
+      environment.systemPackages = with pkgs; [
+        nixos-option
+      ];
+    })
+  ]);
 }
