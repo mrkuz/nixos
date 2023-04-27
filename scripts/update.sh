5303f2f2ce9c20c1fac4104df16fdd0f32f88fe4
mrkuz <markus@bitsandbobs.net>
2023-04-27 22:35:49 +0200

    Pull home-manager repo in update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,38 +2,55 @@
 
 source "${BASH_SOURCE%/*}/lib/common.sh"
 
-BRANCH="nixos-unstable"
-REMOTE="origin"
 NIXPKGS="/nix/nixpkgs"
+NIXPKGS_BRANCH="nixos-unstable"
+
+HOME_MANAGER="/nix/home-manager"
+HOME_MANAGER_BRANCH="master"
 
 git submodule update --recursive --remote
 nix flake update
 git diff flake.lock
+
 command -v niv && niv update
 git diff nix/sources.json
 
-if [[ -e "$NIXPKGS" ]]; then
-  pushd . >& /dev/null
-  cd "$NIXPKGS"
+function pull() {
+  directory=$1
+  branch=$2
 
-  current_branch="$(sudo git symbolic-ref --short HEAD 2>/dev/null)"
-  if [[ $current_branch != "$BRANCH" ]]; then
-    sudo git checkout "$BRANCH"
+  if [[ ! -e "$directory" ]]; then
+    return
   fi
 
-  sudo git fetch $REMOTE
-  if sudo git diff $BRANCH $REMOTE/$BRANCH --quiet --exit-code; then
-    echo "nixpkgs already up to date";
+  pushd . >& /dev/null
+  cd "$directory"
+
+  current_branch="$(sudo git symbolic-ref --short HEAD 2>/dev/null)"
+  if [[ $current_branch != "$branch" ]]; then
+    sudo git checkout "$branch"
+  fi
+
+  sudo git fetch origin
+  if sudo git diff $branch origin/$branch --quiet --exit-code; then
+    echo "$directory already up to date";
   else
-    sudo git diff $BRANCH $REMOTE/$BRANCH --stat
+    sudo git diff $branch origin/$branch --stat
   fi
 
   sudo git pull
   popd
+}
 
+pull $NIXPKGS $NIXPKGS_BRANCH
+
+if [[ -e "$NIXPKGS" ]]; then
   if [[ -e "/etc/nixos/options.json" ]]; then
     nix-build "$NIXPKGS/nixos/release.nix" -A options -o tmp/options
     diff -u --color=always <(jq keys /etc/nixos/options.json) <(jq keys tmp/options/share/doc/nixos/options.json) | less
     rm tmp/options
   fi
 fi
+
+pull $HOME_MANAGER $HOME_MANAGER_BRANCH
+
